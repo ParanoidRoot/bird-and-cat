06cb05cbd8edbc07ffc0e6481f0c7960da9947f2
ParanoidRoot <578791680@qq.com>
2019-12-20 10:01:34 +0800

    Initial commit

---
+++ src/main/java/com/paranoidroot/bird_and_cat/BirdAndCatApplication.java
@@ -0,0 +1,13 @@
+package com.paranoidroot.bird_and_cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdAndCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdAndCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paranoidroot/bird_and_cat/BirdAndCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paranoidroot.bird_and_cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdAndCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
